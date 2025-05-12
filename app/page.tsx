1d0c115b0d76786821a122fcb34a576625d8d700
azissava <azissava231@gmail.com>
2025-05-12 09:20:46 +0700

    uncommand

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-// import Countdown from "@/components/Countdown/Countdown"
-// import Link from "next/link"
-// import Music from "../components/music/music"
-// import FadeContent from "@/components/FadeContent/FadeContent"
-// import AnimatedContent from "@/components/AnimatedContent/AnimatedContent"
+import Countdown from "@/components/Countdown/Countdown"
+import Link from "next/link"
+import Music from "../components/music/music"
+import FadeContent from "@/components/FadeContent/FadeContent"
+import AnimatedContent from "@/components/AnimatedContent/AnimatedContent"
 
-// export default function Home() {
-//   return (
-//     <div>
-//         <div className='relative md:px-96'>
-//          <img src="images/halamanutama.png" alt="" />
-//           <div className="absolute inset-0 md:px-96 px-10 flex justify-center items-center ">
-//             <a href="/undangan" className="md:text-center bg-orange-600 p-2 rounded-xl font-bold">Buka</a>
-//           </div>
-//         </div>
-//     </div>
-//   )
-// }
+export default function Home() {
+  return (
+    <div>
+        <div className='relative md:px-96'>
+         <img src="images/halamanutama.png" alt="" />
+          <div className="absolute inset-0 md:px-96 px-10 flex justify-center items-center ">
+            <a href="/undangan" className="md:text-center bg-orange-600 p-2 rounded-xl font-bold">Buka</a>
+          </div>
+        </div>
+    </div>
+  )
+}
